1d1b8d5a80360cca6d4c72e287d47e95b960277e
leoquiroa <leonelquiroa@gmail.com>
2020-12-27 15:58:54 -0600

    start the face detection

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import cv2
 from pathlib import Path
 
-def create_list(root):
-    file_list = os.listdir(root)
+def video_list(root):
+    file_list = os.listdir(os.path.join(root,'videos'))
     j_list = {}
     for f in file_list:
         first = f.split('_')
@@ -47,7 +47,7 @@ def get_and_save_frames(single_video,subject,q):
         success,image = vidcap.read()
         count += 1
         
-def add_frames_path(root,j_list):
+def frame_list(root,j_list):
     j_frames = {}
     for k_subject,v_subject in j_list.items():
         for k_question,v_question in v_subject.items():
@@ -60,12 +60,38 @@ def add_frames_path(root,j_list):
                 j_frames[k_subject] = {k_question:all_frames}
     return j_frames
 
+def facial_detection(j_frames):
+    url = os.path.join(root,'haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier(url)
+    for k_subject,v_subject in j_frames.items():
+        subject = os.path.join(root,'faces',k_subject)
+        if not os.path.isdir(subject): os.mkdir(subject)
+        for k_question,v_question in v_subject.items():
+            question = os.path.join(subject,k_question)
+            if not os.path.isdir(question): os.mkdir(question)
+            for url in v_question:
+                img = cv2.imread(url)
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                faces = face_cascade.detectMultiScale(gray, 
+                    scaleFactor=1.05, 
+                    minNeighbors=4, 
+                    minSize=(30,30))
+                if len(faces) == 0 : continue
+                for (x, y, w, h) in faces:
+                    crop_img = img[y:y+h, x:x+w].copy()
+                    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                h,tail = os.path.split(url)
+                face_path = os.path.join(question,'square_'+tail)
+                cv2.imwrite(face_path, img)
+                crop_path = os.path.join(question,'crop_'+tail)
+                cv2.imwrite(crop_path, crop_img)
+
 if __name__ == "__main__":
     root = '/home/hitch'
-    j_videos = create_list(os.path.join(root,'videos'))
+    j_videos = video_list(root)
     #analyze_user(root,j_list)
-    j_frames = add_frames_path(root,j_videos)
-    #unique_full_list = sum(all_files,[])
+    j_frames = frame_list(root,j_videos)
+    facial_detection(j_frames)
 
 
 
