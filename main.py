d1492604a193e0bd3f4eb7e1cede619dbfca6a20
leoquiroa <leonelquiroa@gmail.com>
2020-12-27 13:14:03 -0600

    add the literal of the question

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os
 import cv2
+from pathlib import Path
 
 def create_list(root):
     file_list = os.listdir(root)
     j_list = {}
     for f in file_list:
-        arr = f.split('_')
-        if arr[0] in j_list:
-            j_list[arr[0]] += [f]
+        first = f.split('_')
+        user = first[0]
+        question = first[2].split('.')[0]
+        if user in j_list:
+            j_list[user].update({question:f})
         else:
-            j_list[arr[0]] = [f]
+            j_list[user] = {question:f}
     return j_list
 
 def analyze_user(root,j_list):
@@ -23,23 +26,23 @@ def analyze_user(root,j_list):
         analyze_questions(values,subject,root)
 
 def analyze_questions(values,subject,root):
-    for ix,e in enumerate(values):
-        print(ix+1)
+    for q,url in values.items():
+        print(q)
         #question folder
-        question = os.path.join(subject,str(ix+1))
+        question = os.path.join(subject,q)
         if not os.path.isdir(question): os.mkdir(question)
         #video path build and read video
-        single_video = os.path.join(root,'videos',e)
-        get_and_save_frames(single_video,subject,ix)
+        single_video = os.path.join(root,'videos',url)
+        get_and_save_frames(single_video,subject,q)
 
-def get_and_save_frames(single_video,subject,ix):
+def get_and_save_frames(single_video,subject,q):
     #video path build and read video
     vidcap = cv2.VideoCapture(single_video)
     success,image = vidcap.read()
     count = 0
     # video cycle
     while success:
-        frame_path = os.path.join(subject,str(ix+1),str(count)+'.jpg')
+        frame_path = os.path.join(subject,q,str(count)+'.jpg')
         cv2.imwrite(frame_path, image)     # save frame as JPEG file      
         success,image = vidcap.read()
         count += 1
@@ -62,9 +65,9 @@ def cosa(root,j_list):
 if __name__ == "__main__":
     root = '/home/hitch'
     j_list = create_list(os.path.join(root,'videos'))
-    #analyze_user(root,j_list)
-    [j_stats,all_files] = cosa(root,j_list)
-    unique_full_list = sum(all_files,[])
+    analyze_user(root,j_list)
+    #[j_stats,all_files] = cosa(root,j_list)
+    #unique_full_list = sum(all_files,[])
 
 
 
